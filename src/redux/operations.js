4645677c2ead0ef921c1831793035b69bbef891d
vl-brrr <vlada.brayko@gmail.com>
2025-04-28 15:04:51 +0300

    Backend link change

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-axios.defaults.baseURL = 'https://connections-api.herokuapp.com';
+axios.defaults.baseURL = 'https://connections-api.goit.global/';
 
 export const fetchContacts = createAsyncThunk('contacts/fetchAll', async () => {
   const response = await axios.get('/contacts');
